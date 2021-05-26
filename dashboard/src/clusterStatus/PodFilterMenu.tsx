2b9eee6be3d98afe97d09d33bffde5cafaa82b8e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-05-26 13:43:48 +0200

    Add legend in menu

--- dashboard/src/clusterStatus/PodFilterMenu.tsx
+++ dashboard/src/clusterStatus/PodFilterMenu.tsx
@@ -76,6 +76,34 @@ export function PodFilterMenu({
         setValue={(k, v) => setFilterValue("application", k, v)}
       />
       <button onClick={handleClickClear}>Clear</button>
+      <h2>Legend</h2>
+      <div className={"down"}>
+        <span className="dot" /> No contact
+      </div>
+      <div className={"unhealthy"}>
+        <span className="dot" /> Health checks fail
+      </div>
+      <div className={"idle"}>
+        <span className="dot" /> No traffic (&lt; 100)
+      </div>
+      <div className={"busy"}>
+        <span className="dot" /> Normal operation
+      </div>
+      <h2>Numbers are</h2>
+      <div>
+        Uptime (e.g. 3w4d04h12m = 3 weeks, 2 days, 4 hours and 12 minutes). This
+        is kept in full detail so the length of the string gives a visual
+        indication of the uptime
+      </div>
+      <div>
+        Traffic as reported by the underlying pod. Implementation specific for
+        the pod health check
+      </div>
+      <div>
+        Errors in the error log and successful health checks. Reads healthcheck
+        on the format of Dropwizard and reports the failing ones. If any health
+        check fails, the status of the pod becomes yellow
+      </div>
     </>
   );
 }
